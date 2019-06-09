f609f393abc976ad902994965db0d7f837de237c
dedmari <muneer7589@gmail.com>
2019-06-09 20:40:22 +0200

    simple mnist training integrated

---
+++ train_mnist.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+mnist = tf.keras.datasets.mnist
+
+# Loading MNIST dataset
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# Creating model
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(512, activation=tf.nn.relu),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Training model with 5 epochs
+model.fit(x_train, y_train, epochs=5)
+
+# Performing evaluation using test split
+model.evaluate(x_test, y_test)
+
+# Save entire model to HDF5 file
+model.save('./models/trained_model.h5')
