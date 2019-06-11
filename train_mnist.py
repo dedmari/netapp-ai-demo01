fb0925da26ae3a684eed7fe3002f78eb2f5efe35
dedmari <muneer7589@gmail.com>
2019-06-11 11:14:27 +0200

    using rmsprop optimizer

--- train_mnist.py
+++ train_mnist.py
@@ -12,7 +12,7 @@ model = tf.keras.models.Sequential([
   tf.keras.layers.Dropout(0.2),
   tf.keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
-model.compile(optimizer='adam',
+model.compile(optimizer='rmsprop',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
